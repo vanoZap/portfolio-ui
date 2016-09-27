4c830941f7596f72befd2abf7fc4e3efa70c6f48
Glib Marchenko <glibmarchenko@gmail.com>
2016-09-27 18:55:59 +0300

    added animation

--- app/template/controllers/dashboard/dashboard.component.js
+++ app/template/controllers/dashboard/dashboard.component.js
@@ -11,16 +11,72 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var DashboardComponent = (function () {
     function DashboardComponent() {
+        this.menuItems = [
+            {
+                title: 'stat',
+                status: 'out',
+                subMenu: [
+                    {
+                        name: 'map',
+                        title: 'map',
+                        icon: 'fa fa-map-o'
+                    },
+                    {
+                        name: 'chart',
+                        title: 'map',
+                        icon: 'fa fa-line-chart'
+                    }
+                ]
+            },
+            {
+                title: 'prof',
+                status: 'out',
+                subMenu: [
+                    {
+                        name: 'new',
+                        title: 'new',
+                        icon: 'fa fa-plus'
+                    },
+                    {
+                        name: 'edit',
+                        title: 'edit',
+                        icon: 'fa fa-pencil'
+                    }
+                ]
+            }
+        ];
     }
     DashboardComponent.prototype.ngOnInit = function () {
         this.name = 'IG';
         this.profession = 'Web Developer';
         this.avatar = { background: 'url(../asset/img/man.jpg) center center no-repeat' };
     };
+    DashboardComponent.prototype.onShowSubItems = function (item) {
+        item.status = item.status === 'in' ? 'out' : 'in';
+    };
+    DashboardComponent.prototype.onShowItems = function () {
+        this.navStatus = this.navStatus === 'in' ? 'out' : 'in';
+    };
     DashboardComponent = __decorate([
         core_1.Component({
             selector: 'app-dashboard',
-            templateUrl: 'app/template/dashboard.tp.html'
+            templateUrl: 'app/template/dashboard.tp.html',
+            animations: [
+                core_1.trigger('acordion', [
+                    core_1.state('in', core_1.style({ height: '*', overflow: 'hidden' })),
+                    core_1.transition('in => out', core_1.animate('400ms ease-in')),
+                    core_1.transition('out => in', core_1.animate('400ms ease-in')),
+                    core_1.transition('* => void', [
+                        core_1.style({ height: '*' }),
+                        core_1.animate(800, core_1.style({ height: 0 }))
+                    ]),
+                    core_1.state('out', core_1.style({ height: 0, overflow: 'hidden' })),
+                    core_1.transition('* => void', [
+                        core_1.style({ height: 0 }),
+                        core_1.animate(800, core_1.style({ height: '*' }))
+                    ])
+                ])
+            ]
         }), 
         __metadata('design:paramtypes', [])
     ], DashboardComponent);
--- app/template/controllers/dashboard/dashboard.component.ts
+++ app/template/controllers/dashboard/dashboard.component.ts
@@ -1,9 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, trigger, state, style, transition, animate } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-dashboard',
-    templateUrl: 'app/template/dashboard.tp.html'
+    templateUrl: 'app/template/dashboard.tp.html',
+    animations: [
+        trigger('acordion', [
+            state('in', style({height: '*', overflow: 'hidden'})),
+            transition('in => out', animate('400ms ease-in')),
+            transition('out => in', animate('400ms ease-in')),
+            transition('* => void', [
+                style({height: '*'}),
+                animate(800, style({height: 0}))
+            ]),
+            state('out', style({height: 0, overflow: 'hidden'})),
+            transition('* => void', [
+                style({height: 0}),
+                animate(800, style({height: '*'}))
+            ])
+        ])
+    ]
 })
 
 export class DashboardComponent implements OnInit {
@@ -11,6 +27,41 @@ export class DashboardComponent implements OnInit {
     public avatar: Object;
     public name: string;
     public profession: string;
+    public navStatus: 'out';
+    public menuItems = [
+        {
+            title: 'stat',
+            status: 'out',
+            subMenu: [
+                {
+                    name: 'map',
+                    title: 'map',
+                    icon: 'fa fa-map-o'
+                },
+                {
+                    name: 'chart',
+                    title: 'map',
+                    icon: 'fa fa-line-chart'
+                }
+            ]
+        },
+        {
+            title: 'prof',
+            status: 'out',
+            subMenu: [
+                {
+                    name: 'new',
+                    title: 'new',
+                    icon: 'fa fa-plus'
+                },
+                {
+                    name: 'edit',
+                    title: 'edit',
+                    icon: 'fa fa-pencil'
+                }
+            ]
+        }
+    ];
 
     constructor() { }
 
@@ -18,5 +69,13 @@ export class DashboardComponent implements OnInit {
         this.name = 'IG';
         this.profession = 'Web Developer';
         this.avatar = {background: 'url(../asset/img/man.jpg) center center no-repeat'};
+    }
+
+    onShowSubItems(item: any) {
+        item.status = item.status === 'in' ? 'out' : 'in';
+    }
+
+    onShowItems() {
+        this.navStatus = this.navStatus === 'in' ? 'out' : 'in';
     }
 }
